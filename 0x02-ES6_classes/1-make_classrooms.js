9398495edee144d438bb6eb345853af175953541
KwameNtaadu007 <kwamealabama@gmail.com>
2024-03-15 20:15:57 -0700

    classes

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const classA = new ClassRoom(19);
+  const classB = new ClassRoom(20);
+  const classC = new ClassRoom(34);
+  const newClassList = [classA, classB, classC];
+  return (newClassList);
+}
+
