1403fddffb365852a4db1e6d344e42dbb0c260af
KwameNtaadu007 <kwamealabama@gmail.com>
2024-04-30 23:54:23 -0700

    node basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Function displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Emmanuel Ntaadu Gyamfi
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
