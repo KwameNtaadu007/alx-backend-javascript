1e69def6ad22a30044861cfd7a39f28aab46aaac
KwameNtaadu007 <kwamealabama@gmail.com>
2024-05-02 06:01:10 -0700

    refactor package

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,13 +1,15 @@
-const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
+const { expect } = require('chai');
 
-describe('getPaymentTokenFromAPI', () => {
-  it('getPaymentTokenFromAPI(success), where success == true', (done) => {
-    getPaymentTokenFromAPI(true)
-      .then((res) => {
-        expect(res).to.deep.equal({data: 'Successful response from the API'});
-        done();
-      });
+describe('getPaymentTokenFromAPI', function () {
+  describe('arg true', function () {
+    it('resolved correctly', function (done) {
+      getPaymentTokenFromAPI(true)
+        .then((res) => {
+          expect(res).to.include({ data: 'Successful response from the API' });
+          done();
+        })
+        .catch((err) => done(err));
+    });
   });
 });
-
